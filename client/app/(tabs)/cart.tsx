43338bf41b9ebf7a90b544315b74bbb67f62ffd5
Piyush Verma <167591956+CodeSprintX@users.noreply.github.com>
2026-02-23 08:54:16 +0530

    commit

---
+++ client/app/(tabs)/cart.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Cart() {
+  return (
+    <View>
+      <Text>tu pagal hai dusra click kr</Text>
+    </View>
+  )
+}
---
+++ client/app/(tabs)/favorites.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Favorites() {
+  return (
+    <View>
+      <Text>mummy bhi pagal hai</Text>
+    </View>
+  )
+}
